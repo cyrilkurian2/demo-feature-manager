e6a3d4053dba23a2e74054e43160cd879ba5386c
cyrilkurian2 <cyril.kurian@geekywolf.com>
2024-11-12 16:27:09 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import path from 'path';
+import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'user',
+        component: UserComponent,
+        children:[
+            {path: 'login', component: LoginComponent},
+        ]
+        
+    },
+    {path: 'home', component: HomeComponent}
+];
